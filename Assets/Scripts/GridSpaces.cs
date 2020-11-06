c10ba2cd7686a9b5c8f8b5f950c8c6c4775ecb62
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-11-06 15:31:24 +0000

    X button

---
+++ Assets/Scripts/GridSpaces.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpaces : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+    
+
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+
+}
