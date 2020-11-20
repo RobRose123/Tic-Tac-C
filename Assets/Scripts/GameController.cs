a825bc254cce3a96ca943e45ce60e9130ab450c6
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-11-20 16:10:43 +0000

    Winning Condition and Taking Turns (Not Complete) 5:02

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,12 @@ public class GameController : MonoBehaviour
 {
 
     public Text[] buttonList;
+    private string playerSide;
 
     private void Awake()
     {
         SetGameContollerReferenceOnButton();
+        playerSide = "X";
     }
 
     void SetGameContollerReferenceOnButton()
@@ -23,11 +25,22 @@ public class GameController : MonoBehaviour
 
     public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
     public void EndTurn()
     {
-        Debug.Log("EndTurn is not implemented");
+        if(buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        for(int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 }
