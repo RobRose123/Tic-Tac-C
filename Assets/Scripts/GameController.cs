3ec6a9b37b6d6bdcdf83b6ee7ea35d02a7116dc3
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-12-11 15:35:03 +0000

    Game Over Text Complete

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,7 +10,7 @@ public class GameController : MonoBehaviour
     private string playerSide;
 
     public GameObject gameOverPanel;
-    public string gameOverText;
+    public Text gameOverText;
 
     private void Awake()
     {
